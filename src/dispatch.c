aa505cd021caf2092853f3bc576ccc1468cf1e22
Joe Moore <u1917702@live.warwick.ac.uk>
2020-11-22 23:13:40 +0000

    compiling threading funciton (FINALLY)

--- src/dispatch.c
+++ src/dispatch.c
@@ -12,11 +12,15 @@
 struct listOfPackets *packets;
 unsigned long pcount = 0;
 char exitThreads = 0;
+char run = 0;
+
+struct linkedList *newList;
 
 pthread_mutex_t queue_mutex=PTHREAD_MUTEX_INITIALIZER;
 
 void dispatch(struct pcap_pkthdr *header, const unsigned char *packet, int verbose, struct list *list)
 {
+    newList = list;
     unsigned char *newPacket = malloc((sizeof(char)*header->len)+1);
     if(newPacket ==NULL){
         memcpy(newPacket, packet, header->len);
@@ -24,7 +28,7 @@ void dispatch(struct pcap_pkthdr *header, const unsigned char *packet, int verbo
     newPacket[sizeof(char)*header->len] = '\0';
 
     struct listElementPacket *packetToUse = malloc(sizeof(struct listElementPacket));
-    if(packetToUse==NULL) exit(1);
+    if(packetToUse==NULL) exit (1);
 
     packetToUse->header = header;
     packetToUse->packet = packet;
@@ -41,4 +45,39 @@ void recursivelyAddPackets(struct listElementPacket *head, struct listElementPac
     if(head == NULL) head = toAdd;
     else if(head->next == NULL) head->next = toAdd;
     else recursivelyAddPackets(head->next, toAdd);
+}
+
+void *threading(){
+    struct counting *threadCount = malloc(sizeof(struct counting));
+    if (threadCount == NULL) exit(1);
+
+    threadCount->number_of_arp_attacks=0;
+    threadCount->number_of_blacklisted_IDs=0;
+    threadCount->number_of_syn_attacks=0;
+
+    while(run == 0){
+        pthread_mutex_lock(&queue_mutex);
+
+        if(packets->head != NULL){
+            struct listElementPacket *header = packets->head;
+            packets->head = header->next;
+
+            pthread_mutex_unlock(&queue_mutex);
+
+            struct counting *temporaryCount = analyse(header->header, header->packet, 0, newList);
+
+            threadCount->number_of_arp_attacks += temporaryCount->number_of_arp_attacks;
+            threadCount->number_of_blacklisted_IDs += temporaryCount->number_of_blacklisted_IDs;
+            threadCount->number_of_syn_attacks += temporaryCount->number_of_syn_attacks;
+
+            free((void *)header->packet);
+            free(header);
+            free(temporaryCount);
+        }
+        else{
+            pthread_mutex_unlock(&queue_mutex);
+        }
+    }
+
+    return (void *)threadCount;
 }
